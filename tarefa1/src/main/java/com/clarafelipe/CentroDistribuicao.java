f138ed25f7967f8986f7067a1fd6b7d5fb70bf4c
Clara D'Ávila <claracunhadavila@gmail.com>
2022-05-10 01:18:35 -0300

    add

--- tarefa1/src/main/java/com/clarafelipe/CentroDistribuicao.java
+++ tarefa1/src/main/java/com/clarafelipe/CentroDistribuicao.java
@@ -1,222 +1,199 @@
 package com.clarafelipe;
 
 public class CentroDistribuicao {
-    public enum SITUACAO {
-        NORMAL, SOBRAVISO, EMERGENCIA
+  public enum SITUACAO {
+    NORMAL, SOBRAVISO, EMERGENCIA
+  }
+
+  public enum TIPOPOSTO {
+    COMUM, ESTRATEGICO
+  }
+
+  public static final int MAX_ADITIVO = 500;
+  public static final int MAX_ALCOOL = 2500;
+  public static final int MAX_GASOLINA = 10000;
+
+  public int tGasolina;
+  public int tAditivo;
+  public int tAlcool1;
+  public int tAlcool2;
+  public SITUACAO situacao;
+
+  public CentroDistribuicao(int tAditivo, int tGasolina, int tAlcool1, int tAlcool2) {
+    if (tAditivo > MAX_ADITIVO)
+      throw new IllegalArgumentException("Valor Inválido!");
+    else if (tAditivo < 0)
+      throw new IllegalArgumentException("Valor Inválido!");
+    else
+      this.tAditivo = tAditivo;
+
+    if (tGasolina > MAX_GASOLINA)
+      throw new IllegalArgumentException("Valor Inválido!");
+    else if (tGasolina < 0)
+      throw new IllegalArgumentException("Valor Inválido!");
+    else
+      this.tGasolina = tGasolina;
+
+    if (tAlcool1 + tAlcool2 > MAX_ALCOOL)
+      throw new IllegalArgumentException("Valor Inválido!");
+    else if (tAlcool1 < 0 || tAlcool2 < 0)
+      throw new IllegalArgumentException("Valor Inválido!");
+    else if (tAlcool1 != tAlcool2)
+      throw new IllegalArgumentException("Valor Inválido!");
+    else {
+      this.tAlcool1 = tAlcool1;
+      this.tAlcool2 = tAlcool2;
+    }
+    this.defineSituacao();
+  }
+
+  public void defineSituacao() {
+    if (tGasolina < (MAX_GASOLINA * 0.25) || tAditivo < (MAX_ADITIVO * 0.25) || tAlcool1 < ((MAX_ALCOOL / 2) * 0.25)
+        || tAlcool2 < ((MAX_ALCOOL / 2) * 0.25)) {
+      this.situacao = SITUACAO.EMERGENCIA;
+    } else if (tGasolina < (MAX_GASOLINA * 0.5) || tAditivo < (MAX_ADITIVO * 0.5) || tAlcool1 < ((MAX_ALCOOL / 2) * 0.5)
+        || tAlcool2 < ((MAX_ALCOOL / 2) * 0.5)) {
+      this.situacao = SITUACAO.SOBRAVISO;
+    } else {
+      this.situacao = SITUACAO.NORMAL;
+    }
+  }
+
+  public SITUACAO getSituacao() {
+    return this.situacao;
+  }
+
+  public int getGasolina() {
+    return this.tGasolina;
+  }
+
+  public int getAditivo() {
+    return this.tAditivo;
+  }
+
+  public int getAlcool1() {
+    return this.tAlcool1;
+  }
+
+  public int getAlcool2() {
+    return this.tAlcool2;
+  }
+
+  public int recebeAditivo(int qtdade) {
+    if (qtdade <= 0) {
+      return -1;
+    }
+    int vazio = MAX_ADITIVO - this.tAditivo;
+    if (qtdade <= vazio) {
+      return qtdade;
+    } else {
+      return vazio;
+    }
+  }
+
+  public int recebeGasolina(int qtdade) {
+    if (qtdade <= 0) {
+      return -1;
+    }
+    int vazio = MAX_GASOLINA - this.tGasolina;
+    if (qtdade <= vazio) {
+      return qtdade;
+    } else {
+      return vazio;
+    }
+  }
+
+  public int recebeAlcool(int qtdade) {
+    if (qtdade <= 0) {
+      return -1;
+    }
+    int vazio = MAX_ALCOOL - (this.tAlcool1 + this.tAlcool2);
+    if (qtdade <= vazio) {
+      return qtdade;
+    } else {
+      return vazio;
+    }
+  }
+
+  public int[] encomendaCombustivel(int qtdade, TIPOPOSTO tipoPosto) {
+    if (qtdade <= 0 || (tipoPosto != TIPOPOSTO.COMUM && tipoPosto != TIPOPOSTO.ESTRATEGICO)) {
+      return new int[] { -7 };
+    }
+    if (this.situacao == SITUACAO.EMERGENCIA && tipoPosto == TIPOPOSTO.COMUM) {
+      return new int[] { -14 };
     }
 
-    public enum TIPOPOSTO {
-        COMUM, ESTRATEGICO
+    if (this.situacao == SITUACAO.SOBRAVISO && tipoPosto == TIPOPOSTO.COMUM) {
+      return calculaCombustivel((int) (qtdade * 0.5));
+    }
+    if (this.situacao == SITUACAO.EMERGENCIA && tipoPosto == TIPOPOSTO.ESTRATEGICO) {
+      return calculaCombustivelEstrategico(qtdade);
     }
 
-    public static final int MAX_ADITIVO = 500;
-    public static final int MAX_ALCOOL = 2500;
-    public static final int MAX_GASOLINA = 10000;
+    return calculaCombustivel(qtdade);
+  }
 
-    private int tAditivo = 0;
-    private int tGasolina = 0;
-    private int tAlcool1 = 0;
-    private int tAlcool2 = 0;
+  private int[] calculaCombustivel(int qtdade) {
+    double qtdGasolina = qtdade * 0.7;
+    double qtdAlcool = qtdade * 0.25;
+    double qtdAditivo = qtdade * 0.05;
+    if ((int) qtdAditivo > this.tAditivo || (int) qtdAlcool > (this.tAlcool1 + this.tAlcool2)
+        || (int) qtdGasolina > this.tGasolina) {
+      return new int[] { -21 };
+    } else {
+      this.tGasolina = this.tGasolina - (int) qtdGasolina;
+      this.tAditivo = this.tAditivo - (int) qtdAditivo;
 
-    private final int percAditivo = 5;
-    private final int percGasolina = 70;
-    private final int percAlcool = 25;
+      // se der pra dividir igual, tira igual, senão, tira tudo do que menos tem e o
+      // resto do que mais tem
+      double alcoolDivided = qtdAlcool / 2.0;
+      if ((int) alcoolDivided > this.tAlcool1) {
+        this.tAlcool2 = this.tAlcool2 - ((int) qtdAlcool - this.tAlcool1);
+        this.tAlcool1 = 0;
+      } else if ((int) alcoolDivided > this.tAlcool2) {
+        this.tAlcool1 = this.tAlcool1 - ((int) qtdAlcool - this.tAlcool2);
+        this.tAlcool2 = 0;
+      } else {
+        this.tAlcool1 = this.tAlcool1 - (int) alcoolDivided;
+        this.tAlcool2 = this.tAlcool2 - (int) alcoolDivided;
+      }
+      return new int[] { this.tAditivo, this.tGasolina, this.tAlcool1, this.tAlcool2 };
+    }
+  }
 
-    private SITUACAO situacao = SITUACAO.NORMAL;
+  private int[] calculaCombustivelEstrategico(int qtdade) {
+    double qtdGasolina = qtdade * 0.7;
+    double qtdAlcool = qtdade * 0.25;
+    double qtdAditivo = qtdade * 0.05;
+    boolean usaAditivo = true;
 
-    public CentroDistribuicao(int tAditivo, int tGasolina, int tAlcool1, int tAlcool2) {
-        if (tAditivo > MAX_ADITIVO)
-            throw new IllegalArgumentException("Valor Inválido!");
-        else if (tAditivo < 0)
-            throw new IllegalArgumentException("Valor Inválido!");
-        else
-            this.tAditivo = tAditivo;
-
-        if (tGasolina > MAX_GASOLINA)
-            throw new IllegalArgumentException("Valor Inválido!");
-        else if (tGasolina < 0)
-            throw new IllegalArgumentException("Valor Inválido!");
-        else
-            this.tGasolina = tGasolina;
-
-        if (tAlcool1 + tAlcool2 > MAX_ALCOOL)
-            throw new IllegalArgumentException("Valor Inválido!");
-        else if (tAlcool1 < 0 || tAlcool2 < 0)
-            throw new IllegalArgumentException("Valor Inválido!");
-        else if (tAlcool1 != tAlcool2)
-            throw new IllegalArgumentException("Valor Inválido!");
-        else {
-            this.tAlcool1 = tAlcool1;
-            this.tAlcool2 = tAlcool2;
-        }
-
-        this.defineSituacao();
+    if ((int) qtdAditivo > this.tAditivo) {
+      qtdGasolina = qtdade * 0.75;
+      qtdAlcool = qtdade * 0.25;
+      usaAditivo = false;
     }
 
-    public void defineSituacao() {
-        if (this.getAditivo() < 0.25 * MAX_ADITIVO ||
-                this.getGasolina() < 0.25 * MAX_GASOLINA ||
-                this.getAlcool1() < 0.25 * MAX_ALCOOL / 2 ||
-                this.getAlcool2() < 0.25 * MAX_ALCOOL / 2) {
-            this.situacao = SITUACAO.EMERGENCIA;
-        } else if (this.getAditivo() < 0.5 * MAX_ADITIVO ||
-                this.getGasolina() < 0.5 * MAX_GASOLINA ||
-                this.getAlcool1() < 0.5 * MAX_ALCOOL / 2 ||
-                this.getAlcool2() < 0.5 * MAX_ALCOOL / 2) {
-            this.situacao = SITUACAO.SOBRAVISO;
-        } else {
-            this.situacao = SITUACAO.NORMAL;
-        }
+    if ((int) qtdAlcool > (this.tAlcool1 + this.tAlcool2) || (int) qtdGasolina > this.tGasolina) {
+      return new int[] { -21 };
+    } else {
+      if (usaAditivo)
+        this.tAditivo = this.tAditivo - (int) qtdAditivo;
+      this.tGasolina = this.tGasolina - (int) qtdGasolina;
+
+      // se der pra dividir igual, tira igual, senão, tira tudo do que menos tem e o
+      // resto do que mais tem
+      double alcoolDivided = qtdAlcool / 2.0;
+      if ((int) alcoolDivided > this.tAlcool1) {
+        this.tAlcool2 = this.tAlcool2 - ((int) qtdAlcool - this.tAlcool1);
+        this.tAlcool1 = 0;
+      } else if ((int) alcoolDivided > this.tAlcool2) {
+        this.tAlcool1 = this.tAlcool1 - ((int) qtdAlcool - this.tAlcool2);
+        this.tAlcool2 = 0;
+      } else {
+        this.tAlcool1 = this.tAlcool1 - (int) alcoolDivided;
+        this.tAlcool2 = this.tAlcool2 - (int) alcoolDivided;
+      }
+      return new int[] { this.tAditivo, this.tGasolina, this.tAlcool1, this.tAlcool2 };
     }
-
-    public SITUACAO getSituacao() {
-        return this.situacao;
-    }
-
-    public int getGasolina() {
-        return this.tGasolina;
-    }
-
-    public void setGasolina(int subGasolina) {
-        this.tGasolina -= subGasolina;
-    }
-
-    public void setAditivo(int subAditivo) {
-        this.tAditivo -= subAditivo;
-    }
-
-    public void setAlcool1(int subAlcool) {
-        this.tAlcool1 -= subAlcool;
-    }
-
-    public void setAlcool2(int subAlcool) {
-        this.tAlcool2 -= subAlcool;
-    }
-
-    public int getAditivo() {
-        return this.tAditivo;
-    }
-
-    public int getAlcool1() {
-        return this.tAlcool1;
-    }
-
-    public int getAlcool2() {
-        return this.tAlcool2;
-    }
-
-    public int recebeAditivo(int qtdade) {
-        if (qtdade < 0)
-            return -1;
-
-        int espacoTanque = MAX_ADITIVO - this.getAditivo();
-
-        if (espacoTanque == 0)
-            return 0;
-        if (qtdade >= espacoTanque) {
-            this.tAditivo += espacoTanque;
-            return (qtdade - espacoTanque);
-        }
-
-        this.tAditivo += qtdade;
-        return qtdade;
-    }
-
-    public int recebeGasolina(int qtdade) {
-        if (qtdade < 0)
-            return -1;
-
-        int espacoTanque = MAX_GASOLINA - this.getGasolina();
-
-        if (espacoTanque == 0)
-            return 0;
-        if (qtdade >= espacoTanque) {
-            this.tGasolina += espacoTanque;
-            return (qtdade - espacoTanque);
-        }
-
-        this.tGasolina += qtdade;
-        return qtdade;
-    }
-
-    public int recebeAlcool(int qtdade) {
-        if (qtdade < 0)
-            return -1;
-
-        int espacoTanque = MAX_ALCOOL - this.getAlcool1() - this.getAlcool2();
-
-        if (espacoTanque == 0)
-            return 0;
-        if (qtdade >= espacoTanque) {
-            this.tAlcool1 += espacoTanque / 2;
-            this.tAlcool2 += espacoTanque / 2;
-            return (qtdade - espacoTanque);
-        }
-
-        this.tAlcool1 += qtdade / 2;
-        this.tAlcool2 += qtdade / 2;
-        return qtdade - (qtdade % 2);
-    }
-
-    public int[] encomendaCombustivel(int qtdade, TIPOPOSTO tipoPosto) {
-        if (qtdade < 0)
-            return new int[] { -7, 0, 0, 0 };
-
-        int qtdRequeridaAditivo = (int) (percAditivo * qtdade) / 100;
-        int qtdRequeridaGasolina = (int) (percGasolina * qtdade) / 100;
-        int qtdRequeridaAlcool = (int) (percAlcool * qtdade) / 100;
-
-        if (qtdRequeridaAditivo > getAditivo() ||
-                qtdRequeridaGasolina > getGasolina() ||
-                qtdRequeridaAlcool > getAlcool1() + getAlcool2())
-            return new int[] { -21, 0, 0, 0 };
-
-        if (getSituacao() == SITUACAO.NORMAL) {
-            setAditivo(qtdRequeridaAditivo);
-            setGasolina(qtdRequeridaGasolina);
-            setAlcool1((int) qtdRequeridaAlcool / 2);
-            setAlcool2((int) qtdRequeridaAlcool / 2);
-            return new int[] { qtdRequeridaAditivo, qtdRequeridaGasolina,
-                    (int) (qtdRequeridaAlcool / 2), (int) (qtdRequeridaAlcool / 2) };
-        }
-
-        if (getSituacao() == SITUACAO.SOBRAVISO) {
-            if (tipoPosto == TIPOPOSTO.COMUM) {
-                setAditivo((int) 0.5 * qtdRequeridaAditivo);
-                setGasolina((int) 0.5 * qtdRequeridaGasolina);
-                setAlcool1((int) 0.5 * qtdRequeridaAlcool / 2);
-                setAlcool2((int) 0.5 * qtdRequeridaAlcool / 2);
-
-                return new int[] { (int) 0.5 * qtdRequeridaAditivo, (int) 0.5 * qtdRequeridaGasolina,
-                        (int) (0.5 * qtdRequeridaAlcool / 2), (int) (0.5 * qtdRequeridaAlcool / 2) };
-            }
-
-            if (tipoPosto == TIPOPOSTO.ESTRATEGICO) {
-                setAditivo(qtdRequeridaAditivo);
-                setGasolina(qtdRequeridaGasolina);
-                setAlcool1((int) qtdRequeridaAlcool / 2);
-                setAlcool2((int) qtdRequeridaAlcool / 2);
-                return new int[] { qtdRequeridaAditivo, qtdRequeridaGasolina,
-                        (int) (qtdRequeridaAlcool / 2), (int) (qtdRequeridaAlcool / 2) };
-            }
-        }
-
-        if (getSituacao() == SITUACAO.EMERGENCIA) {
-            if (tipoPosto == TIPOPOSTO.COMUM) {
-                return new int[] { -14, 0, 0, 0 };
-            }
-
-            if (tipoPosto == TIPOPOSTO.ESTRATEGICO) {
-                setAditivo((int) 0.5 * qtdRequeridaAditivo);
-                setGasolina((int) 0.5 * qtdRequeridaGasolina);
-                setAlcool1((int) 0.5 * qtdRequeridaAlcool / 2);
-                setAlcool2((int) 0.5 * qtdRequeridaAlcool / 2);
-
-                return new int[] { (int) 0.5 * qtdRequeridaAditivo, (int) 0.5 * qtdRequeridaGasolina,
-                        (int) (0.5 * qtdRequeridaAlcool / 2), (int) (0.5 * qtdRequeridaAlcool / 2) };
-            }
-        }
-
-        return new int[] { 0 };
-    }
+  }
 }
--- tarefa1/src/test/java/com/clarafelipe/CentroDistribuicaoTest.java
+++ tarefa1/src/test/java/com/clarafelipe/CentroDistribuicaoTest.java
@@ -1,162 +1,165 @@
 package com.clarafelipe;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 //import static org.junit.Assert.assertThrows;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import com.clarafelipe.CentroDistribuicao.TIPOPOSTO;
 
 import org.junit.Test;
 
 public class CentroDistribuicaoTest {
 
+    @ParameterizedTest
+    @CsvSource(value = {
+            "250,5000,625,625,NORMAL",
+            "499,9999,1249,1249,NORMAL",
+            "249,10000,1250,1250,SOBRAVISO",
+            "500,4999,1250,1250,SOBRAVISO",
+            "500,10000,624,1250,SOBRAVISO",
+            "500,10000,1250,624,SOBRAVISO",
+            "124,10000,1250,1250,EMERGENCIA",
+            "500,2499,1250,1250,EMERGENCIA",
+            "500,10000,312,1250,EMERGENCIA",
+            "500,10000,1250,312,EMERGENCIA" })
+    public void shouldConstructWithStatus(String tAditivo, String tGasolina, String tAlcool1, String tAlcool2, String status) {
+        CentroDistribuicao cd = new CentroDistribuicao(Integer.parseInt(tAditivo), Integer.parseInt(tGasolina),
+                Integer.parseInt(tAlcool1), Integer.parseInt(tAlcool2));
+        assertEquals(status, cd.getSituacao().toString());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = { "250,5000,625,625,NORMAL",
+            "249,10000,1250,1250,SOBRAVISO",
+            "124,10000,1250,1250,EMERGENCIA" })
+    public void shouldDefineSituation(String tAditivo, String tGasolina, String tAlcool1, String tAlcool2, String status) {
+        CentroDistribuicao cd = new CentroDistribuicao(Integer.parseInt(tAditivo), Integer.parseInt(tGasolina),
+                Integer.parseInt(tAlcool1), Integer.parseInt(tAlcool2));
+        cd.defineSituacao();
+        assertEquals(status, cd.getSituacao().toString());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "300,5000,625,625,100,100",
+            "400,5000,625,625,200,100",
+            "400,5000,625,625,-10,-1",
+            "400,5000,625,625,0,-1" })
+    public void shouldReciveAdictive(String tAditivo, String tGasolina, String tAlcool1, String tAlcool2, String qtd, String expected) {
+        CentroDistribuicao cd = new CentroDistribuicao(Integer.parseInt(tAditivo), Integer.parseInt(tGasolina),
+                Integer.parseInt(tAlcool1), Integer.parseInt(tAlcool2));
+        int result = cd.recebeAditivo(Integer.parseInt(qtd));
+        assertEquals(Integer.parseInt(expected), result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "300,8000,625,625,1000,1000",
+            "400,9000,625,625,2000,1000",
+            "400,8000,625,625,-10,-1",
+            "400,8000,625,625,0,-1" })
+    public void shouldReciveGasoline(String tAditivo, String tGasolina, String tAlcool1, String tAlcool2, String qtd, String expected) {
+        CentroDistribuicao cd = new CentroDistribuicao(Integer.parseInt(tAditivo), Integer.parseInt(tGasolina),
+                Integer.parseInt(tAlcool1), Integer.parseInt(tAlcool2));
+        int result = cd.recebeGasolina(Integer.parseInt(qtd));
+        assertEquals(Integer.parseInt(expected), result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "400,9000,900,900,400,400",
+            "400,9000,1100,1100,400,300",
+            "400,8000,1250,1100,400,150",
+            "400,8000,1250,1100,100,100",
+            "400,8000,1100,1100,-10,-1",
+            "400,8000,1100,1100,0,-1" })
+    public void shouldReciveAlcool(String tAditivo, String tGasolina, String tAlcool1, String tAlcool2, String qtd, String expected) {
+        CentroDistribuicao cd = new CentroDistribuicao(Integer.parseInt(tAditivo), Integer.parseInt(tGasolina),
+                Integer.parseInt(tAlcool1), Integer.parseInt(tAlcool2));
+        int result = cd.recebeAlcool(Integer.parseInt(qtd));
+        assertEquals(Integer.parseInt(expected), result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "500,10000,1250,1250,0,COMUM,-7,-7,-7,-7",
+            "500,10000,1250,1250,20000,COMUM,-21,-21,-21,-21",
+            "500,10000,1250,1250,7650,COMUM,118,4645,294,294",
+            "250,4000,625,625,2000,COMUM,200,3300,500,500",
+            "250,2400,625,625,3000,COMUM,-14,-14,-14,-14",
+            "500,10000,1250,1250,7650,ESTRATEGICO,118,4645,294,294",
+            "250,4000,625,625,2000,ESTRATEGICO,150,2600,375,375",
+            "250,2400,625,625,3000,ESTRATEGICO,100,300,250,250",
+            "100,2400,625,625,3000,ESTRATEGICO,100,150,250,250",
+            // extras adicionados após code coverage
+            "250,2400,125,1125,3000,ESTRATEGICO,100,300,0,500",
+            "250,2400,1125,125,3000,ESTRATEGICO,100,300,500,0",
+            "500,7000,625,1250,6000,COMUM,200,2800,0,375",
+            "500,7000,1250,625,6000,COMUM,200,2800,375,0",
+            "250,2400,125,1125,10000,ESTRATEGICO,-21,-21,-21,-21",
+    })
+    public void shouldEncomendFuel(String tAditivo, String tGasolina, String tAlcool1, String tAlcool2, String qtd, String posto,
+            String expected1, String expected2, String expected3, String expected4) {
+        CentroDistribuicao cd = new CentroDistribuicao(Integer.parseInt(tAditivo), Integer.parseInt(tGasolina),
+                Integer.parseInt(tAlcool1), Integer.parseInt(tAlcool2));
+        TIPOPOSTO tipoPosto;
+
+        // MAP THE TIPOPOSTO VARIABLE
+        if (posto.equals("ESTRATEGICO")) {
+            tipoPosto = CentroDistribuicao.TIPOPOSTO.ESTRATEGICO;
+        } else {
+            tipoPosto = CentroDistribuicao.TIPOPOSTO.COMUM;
+        }
+
+        // CHECK ARRAY SIZE
+        int tamanho = 0;
+        if (Integer.parseInt(expected1) < 0) {
+            tamanho = 1;
+        } else {
+            tamanho = 4;
+        }
+
+        // CREATE ARRAY WITH THE RIGHT SIZE
+        int[] expected = new int[tamanho];
+        if (Integer.parseInt(expected1) < 0) {
+            expected = new int[] { Integer.parseInt(expected1) };
+        } else {
+            expected = new int[] { Integer.parseInt(expected1), Integer.parseInt(expected2),
+                    Integer.parseInt(expected3), Integer.parseInt(expected4) };
+        }
+
+        // CALLS FUNCTION AND ASSERTS IT
+        int[] result = cd.encomendaCombustivel(Integer.parseInt(qtd), tipoPosto);
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], result[i]);
+        }
+    }
+
+    // extras adicionados após code coverage
     @Test
-    public void qtdParametroInvalida() {
-        CentroDistribuicao centroDistribuicao = new CentroDistribuicao(1000, 1500, 1500, 1005);
-        int[] resObtida = centroDistribuicao.encomendaCombustivel(-50, CentroDistribuicao.TIPOPOSTO.COMUM);
-        assertEquals(-7, resObtida[0]);
+    public void shouldGetGasolina() {
+        CentroDistribuicao cd = new CentroDistribuicao(0, 2000, 0, 0);
+        assertEquals(2000, cd.getGasolina());
     }
 
     @Test
-    public void pedidoNaoPodeSerAtendido() {
-        CentroDistribuicao centroDistribuicao = new CentroDistribuicao(0, 2000, 200, 6000);
-        int[] resObtida = centroDistribuicao.encomendaCombustivel(-50, CentroDistribuicao.TIPOPOSTO.COMUM);
-        assertEquals(-14, resObtida[0]);
+    public void shouldGetAlcool1() {
+        CentroDistribuicao cd = new CentroDistribuicao(0, 0, 1000, 0);
+        assertEquals(1000, cd.getAlcool1());
     }
 
     @Test
-    public void faltaAditivo() {
-        CentroDistribuicao centroDistribuicao = new CentroDistribuicao(400, 9000, 1000, 1000);
-        int[] resObtida = centroDistribuicao.encomendaCombustivel(-50, CentroDistribuicao.TIPOPOSTO.COMUM);
-        assertEquals(-21, resObtida[0]);
+    public void shouldGetAlcool2() {
+        CentroDistribuicao cd = new CentroDistribuicao(0, 0, 0, 1000);
+        assertEquals(1000, cd.getAlcool2());
     }
 
     @Test
-    public void faltaAlcool1() {
-        CentroDistribuicao centroDistribuicao = new CentroDistribuicao(400, 9000, 1000, 1000);
-        int[] resObtida = centroDistribuicao.encomendaCombustivel(-50, CentroDistribuicao.TIPOPOSTO.COMUM);
-        assertEquals(-21, resObtida[0]);
-    }
-
-    @Test
-    public void faltaAlcool2() {
-        CentroDistribuicao centroDistribuicao = new CentroDistribuicao(400, 9000, 1000, 1000);
-        int[] resObtida = centroDistribuicao.encomendaCombustivel(-50, CentroDistribuicao.TIPOPOSTO.COMUM);
-        assertEquals(-21, resObtida[0]);
-    }
-
-    @Test
-    public void faltaGasolina() {
-        CentroDistribuicao centroDistribuicao = new CentroDistribuicao(400, 9000, 1000, 1000);
-        int[] resObtida = centroDistribuicao.encomendaCombustivel(-50, CentroDistribuicao.TIPOPOSTO.COMUM);
-        assertEquals(-21, resObtida[0]);
-    }
-
-    @Test
-    public void qtdAlcoolDiferentes() {//IllegalArgumentException
-        CentroDistribuicao centroDistribuicao = new CentroDistribuicao(400, 9000, 1000, 1000);
-        int[] resObtida = centroDistribuicao.encomendaCombustivel(-50, CentroDistribuicao.TIPOPOSTO.COMUM);
-        assertEquals(-21, resObtida[0]);
-    }
-
-    @Test
-    public void situacaoNormal() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void situacaoSobraviso() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void situacaoEmergencia() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void situacaoNormalLimiteOn() {
-        CentroDistribuicao centroDistribuicao = new CentroDistribuicao(450, 0, 0, 0);
-        assertEquals(50, centroDistribuicao.recebeAditivo(51));
-    }
-
-    @Test
-    public void situacaoNormalLimiteOff() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void situacaoSobravisoLimiteOn() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void situacaoSobravisoLimiteOff() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void situacaoEmergenciaLimiteOn() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void situacaoEmergenciaLimiteOff() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void situacaoNormalLimiteIn() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void situacaoSobravisoLimiteIn() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void situacaoEmergenciaLimiteIn() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void qtdAditivoInvalida() {//IllegalArgumentException
-        assertTrue(true);
-    }
-
-    @Test
-    public void qtdAlcool1Invalida() {//IllegalArgumentException
-        assertTrue(true);
-    }
-
-    @Test
-    public void qtdAlcool2Invalida() {//IllegalArgumentException
-        assertTrue(true);
-    }
-
-    @Test
-    public void qtdGasolinaInvalida() {//IllegalArgumentException
-        assertTrue(true);
-    }
-
-    @Test
-    public void qtdValidaAditivo() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void qtdValidaAlcool1() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void qtdValidaAlcool2() {
-        CentroDistribuicao centroDistribuicao = new CentroDistribuicao(0, 0, 1200, 1200);
-        assertEquals(100, centroDistribuicao.recebeAlcool(102));
-    }
-
-    @Test
-    public void qtdValidaGasolina() {
-        assertTrue(true);
+    public void shouldGetAditivo() {
+        CentroDistribuicao cd = new CentroDistribuicao(150, 0, 0, 0);
+        assertEquals(150, cd.getAditivo());
     }
 
 }
