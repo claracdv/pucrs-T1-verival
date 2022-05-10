29bd912bb049493d7f528fdce6c2620deadc5a5c
Clara D'Ávila <claracunhadavila@gmail.com>
2022-05-09 22:17:36 -0300

    Cria casos de teste

--- tarefa1/src/test/java/com/clarafelipe/CentroDistribuicaoTest.java
+++ tarefa1/src/test/java/com/clarafelipe/CentroDistribuicaoTest.java
@@ -1,20 +1,146 @@
 package com.clarafelipe;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class CentroDistribuicaoTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class CentroDistribuicaoTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void qtdParametroInvalida() {
+        assertTrue(true);
     }
+
+    @Test
+    public void pedidoNaoPodeSerAtendido() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void semCombustivelSuficiente() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void faltaAditivo() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void faltaAlcool1() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void faltaAlcool2() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void faltaGasolina() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void situacaoNormal() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void situacaoSobraviso() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void situacaoEmergencia() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void situacaoNormalLimiteOn() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void situacaoNormalLimiteOff() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void situacaoSobravisoLimiteOn() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void situacaoSobravisoLimiteOff() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void situacaoEmergenciaLimiteOn() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void situacaoEmergenciaLimiteOff() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void situacaoNormalLimiteIn() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void situacaoSobravisoLimiteIn() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void situacaoEmergenciaLimiteIn() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void qtdAditivoInvalida() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void qtdAlcool1Invalida() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void qtdAlcool2Invalida() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void qtdGasolinaInvalida() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void qtdValidaAditivo() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void qtdValidaAlcool1() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void qtdValidaAlcool2() {
+        CentroDistribuicao centroDistribuicao = new CentroDistribuicao(0, 0, 1200, 1200);
+        assertEquals(100, centroDistribuicao.recebeAlcool(102));
+    }
+
+    @Test
+    public void qtdValidaGasolina() {
+        assertTrue(true);
+    }
+
 }
